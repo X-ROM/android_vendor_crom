AfterlifeOS/vendor_afterlife

Don't assume phones are the only clients for the telephony configuration bits
Data-enabled tablets need love, too

Change-Id: I239823f198260d0f045c6e7305cf04082a8ef1e9

config/telephony.mk
@@ -1,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/cm/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+

X-ROM/android_vendor_crom

Conflicts:
	config/common_phone.mk

config/telephony.mk
@@ -1,0 +1,12 @@
+# World APN list
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/etc/apns-conf.xml:system/etc/apns-conf.xml
+
+# World SPN overrides list
+PRODUCT_COPY_FILES += \
+    vendor/crom/prebuilt/common/etc/spn-conf.xml:system/etc/spn-conf.xml
+
+# SIM Toolkit
+PRODUCT_PACKAGES += \
+    Stk
+