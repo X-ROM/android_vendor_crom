e54dece10a55643f826394324d001978d06b4fc6
winner00 <toverturf@gmail.com>
2013-11-27 19:56:25 -0700

    Fix superuser
    Change-Id: I35c30a52c247b63976342e11859f5d8acd9b703b

--- config/common.mk
+++ config/common.mk
@@ -3,7 +3,6 @@ PRODUCT_BRAND ?= C-RoM
 
 #SuperUser
 SUPERUSER_EMBEDDED := true
-SUPERUSER_PACKAGE_PREFIX := com.android.settings.cyanogenmod.superuser
 
 # overrides
 PRODUCT_PROPERTY_OVERRIDES += \
