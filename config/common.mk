8e88561aee5da9cd997f65ef73b35ad02d3877c2
Jake Weinstein <xboxfanj@msn.com>
2013-12-04 16:17:28 -0600

    vendor: Add Qualcomm's optimized dalvik from Moto X to QCOM devices  @ krzaq @ ctfrommn @ MrBaNkS
    Change-Id: Iac8da6a3a52c47deca1aabde31f5d6899fbd8de6

--- config/common.mk
+++ config/common.mk
@@ -203,3 +203,15 @@ ro.romstats.ga=UA-20205669-1 \
 ro.romstats.name=C-RoM \
 ro.romstats.url=http://stats.c-rom.org \
 ro.romstats.version=$(CROM_VERSION)
+
+# Qualcomm Optimized Dalvik
+LOCAL_PATH:= vendor/crom/prebuilt
+
+ifneq ($(filter msm8610 msm8x26 msm8226 msm8x74 msm8974 msm8960 msm8660 msm7627a msm7630_surf apq8084 mpq8092,$(TARGET_BOARD_PLATFORM)),)
+PRODUCT_COPY_FILES += \
+        $(LOCAL_PATH)/bin/dalvikvm:system/bin/dalvikvm \
+        $(LOCAL_PATH)/bin/dexopt:system/bin/dexopt \
+        $(LOCAL_PATH)/lib/libcutils.so:system/lib/libcutils.so \
+        $(LOCAL_PATH)/lib/libdvm.so:system/lib/libdvm.so \
+        $(LOCAL_PATH)/lib/libqc-opt.so:system/lib/libqc-opt.so
+endif
