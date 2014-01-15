c522be6289537a5f853a507b94463677347e2a86
aways <aways@c-rom.org>
2014-01-16 00:45:57 +0100

    cleanup

--- config/common_phone.mk
+++ config/common_phone.mk
@@ -4,10 +4,6 @@ include frameworks/base/data/sounds/NewAudio.mk
 # common
 $(call inherit-product, vendor/crom/config/common.mk)
 
-# media
-PRODUCT_COPY_FILES += \
-    vendor/crom/prebuilt/common/media/hdpi/bootanimation.zip:system/media/bootanimation.zip
-
 # ringtones
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.config.ringtone=Hydra.ogg \
