b6317a1b987a75ac5fbb37b19fc3aa551190aa74
aways <aways@c-rom.org>
2014-02-10 01:11:05 +0100

    Fixup

--- config/common.mk
+++ config/common.mk
@@ -143,7 +143,7 @@ PRODUCT_COPY_FILES += \
 
 # Catlog Logcat reader
 PRODUCT_COPY_FILES += \
-    vendor/crom/prebuilt/common/app/Catlog_v_1_4.apk:/system/app/Catlog_v_1_4.apk
+    vendor/crom/prebuilt/common/app/Catlog.apk:system/app/Catlog.apk
 # SELinux filesystem labels
 PRODUCT_COPY_FILES += \
     vendor/crom/prebuilt/common/etc/init.d/50selinuxrelabel:system/etc/init.d/50selinuxrelabel
