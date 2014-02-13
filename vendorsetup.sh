93d6f709dac7c04bf74262f314dedcdc224c751c
aways <aways@c-rom.org>
2014-02-13 23:48:42 +0100

    jflte

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,4 @@
 add_lunch_combo crom_galaxysmtd-userdebug
 add_lunch_combo crom_mako-userdebug
-add_lunch_combo crom_jfltexx-userdebug
+add_lunch_combo crom_jflte-userdebug
 add_lunch_combo crom_m7-userdebug
