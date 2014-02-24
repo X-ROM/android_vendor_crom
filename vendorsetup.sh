cdbf1f09b44c5e62508b5003a3370802950063a9
aways <aways@c-rom.org>
2014-02-24 22:49:35 +0100

    add fascinatemtd

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,4 +1,5 @@
 add_lunch_combo crom_galaxysmtd-userdebug
+add_lunch_combo crom_fascinatemtd-userdebug
 add_lunch_combo crom_mako-userdebug
 add_lunch_combo crom_jflte-userdebug
 add_lunch_combo crom_m7-userdebug
