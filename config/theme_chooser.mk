4f5b616883baf3534e6a94f9345f632813e4400f
Clark Scheff <clark@cyngn.com>
2014-04-28 12:03:07 -0700

    CM11 Themes: Add feature for new themes.
    Change-Id: I0da30606534c13a43de33bef08f79fbbb1b348d3

--- config/theme_chooser.mk
+++ config/theme_chooser.mk
@@ -6,5 +6,6 @@ PRODUCT_PACKAGES += \
 
 # theme chooser
 PRODUCT_COPY_FILES += \
-       vendor/crom/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml
+       vendor/crom/config/permissions/com.tmobile.software.themes.xml:system/etc/permissions/com.tmobile.software.themes.xml \
+       vendor/crom/config/permissions/org.cyanogenmod.theme.xml:system/etc/permissions/org.cyanogenmod.theme.xml
 
